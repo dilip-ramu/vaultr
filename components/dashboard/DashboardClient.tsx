4b22abaa0e2453bcb4c6457494b21a357293adbe
Dilip <diliptr.in@gmail.com>
2026-06-24 11:45:39 +0530

    Bump version to v2.7 (trigger deploy)

--- components/dashboard/DashboardClient.tsx
+++ components/dashboard/DashboardClient.tsx
@@ -573,7 +573,7 @@ export default function DashboardClient({
 
       {/* Version stamp */}
       <p className="text-center text-[10px] pb-4" style={{ color: 'var(--text-faint)' }}>
-        v2.6
+        v2.7
       </p>
 
       {showAddTx && (
